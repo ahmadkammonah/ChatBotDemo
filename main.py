9ebfc3c3d316a3029f45b5e98b04855cc894eb6f
ahmadkammonah <ahmad.kammonah@gmail.com>
2024-07-08 09:29:04 -0300

    Changed the interface slightly

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 # Import Libraries
 import streamlit as st
-from streamlit_chat import message
+import time
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
+
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-## function to load Gemini Pro model and get response
-model= genai.GenerativeModel("gemini-1.5-flash")
+# Function to load Gemini Pro model and get response
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat()
 
-#TODO: Add history funcionality
-def get_gemini_response(question, history):
-    chat = model.start_chat(history=[])
-    response=chat.send_message(question)
-    for chunk in response:
-        output = chunk.text
-    return output
 
-#TODO: Add functionality to submit images.
-def on_Submit():
+# TODO: Add history functionality
+def get_gemini_response(question):
+    text_response = []
+    responses = chat.send_message(question, stream=True)
+    for chunk in responses:
+        text_response.append(chunk.text)
+    streamed_output = "".join(text_response)
+
+    for word in streamed_output.split():
+        yield word + " "
+        time.sleep(0.005)
+
+
+# TODO: Add functionality to submit images.
+def on_submit():
     user_input = st.session_state.user_input
     output = get_gemini_response(user_input, st.session_state['past'])
     st.session_state['past'].append(user_input)
@@ -27,34 +35,47 @@ def on_Submit():
 
     st.session_state.user_input = ''
 
-def on_Clear():
-    del st.session_state.past[:]
-    del st.session_state.generated[:]
+
+def on_clear():
+    del st.session_state.messages[:]
 
 
 if __name__ == '__main__':
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything 🤗"]
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
 
-    st.title("ChatBot")
-    response_container = st.empty()
-
-
-    # container for the chat history
-    response_container = st.container()
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
-
-    # container for the user's text input
     user_container = st.container()
     with user_container:
-        st.button("Clear message", on_click=on_Clear)
-        st.text_input('User Input:', key='user_input', on_change=on_Submit)
+        st.title("💬 Chatbot")
+        st.write(
+            "This is a simple chatbot that uses Google's Gemini-1.5-flash to generate responses. "
+        )
+        st.button("Clear message", on_click=on_clear)
 
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
+    chat_container = st.container()
 
+    # Display the existing chat messages via `st.chat_message`.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page.
+    if prompt := st.chat_input("What is up?"):
+        # Store and display the current prompt.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        print(st.session_state.messages)
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Generate a response using the OpenAI API.
+        stream = get_gemini_response(prompt)
+
+        # Stream the response to the chat using `st.write_stream`, then store it in
+        # session state.
+        with st.chat_message("assistant"):
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
